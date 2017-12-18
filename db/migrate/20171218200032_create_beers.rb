b6c95860f85f43232b4a8b54523618e263416d3b
Janusz Szubert <theinvalidnonce@gmail.com>
2017-12-18 20:50:19 +0000

    Add columns to user, beer, brewery models

--- db/migrate/20171218200032_create_beers.rb
+++ db/migrate/20171218200032_create_beers.rb
@@ -1,7 +1,9 @@
 class CreateBeers < ActiveRecord::Migration
   def change
     create_table :beers do |t|
-
+      t.string :name
+      t.string :type
+      t.integer :abv
       t.timestamps null: false
     end
   end
--- db/migrate/20171218200929_create_breweries.rb
+++ db/migrate/20171218200929_create_breweries.rb
@@ -1,7 +1,7 @@
 class CreateBreweries < ActiveRecord::Migration
   def change
     create_table :breweries do |t|
-
+      t.string :name
       t.timestamps null: false
     end
   end
