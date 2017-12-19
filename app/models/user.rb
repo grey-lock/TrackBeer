5ca0208b38a06d2964880378f47903bc57fd1ce5
Janusz Szubert <theinvalidnonce@gmail.com>
2017-12-19 23:13:04 +0000

    Add user validations, feature ideas, fix brewery association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   has_many :beers
-  has_many :breweries
+  has_many :breweries, through: :beers # Users can have favorite breweries that they can add through the beers they like
   
   has_secure_password
   
   validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
-  validates :email, presence: true, uniqueness: true
-  validates :password_digest, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 4 } # TODO: Add a regex? to validate the emails with @ and more.
+  validates :password_digest, presence: true, length: { minimum: 6 } # TODO: Enforce stronger pass security such as have symbol/number
 end
