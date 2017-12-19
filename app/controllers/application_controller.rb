57e60f9c00130f306e1e7b053be7555bb4a54791
Janusz Szubert <theinvalidnonce@gmail.com>
2017-12-19 00:29:01 +0000

    Add helper methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,34 @@ class ApplicationController < Sinatra::Base
   
   configure do
     enable :sessions unless test?
-    set :session_secret, "super_secure_session_secret" # TODO: generate random session secret in ENV
+    set :session_secret, "super_secure_session_secret" # TODO: generate sysrandom session secret in ENV
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
   get "/" do
     erb :welcome
+  end
+  
+  ## Helpers ##
+  
+  helpers do 
+    
+    # If logged in, NOT NOT the current_user
+    def logged_in?
+      !!current_user
+    end
+    
+    # Find the current_user by finding their session[:user_id] if one exists #
+    def current_user
+      @current_user = User.find(session[:user_id]) if session[:user_id]
+    end
+    
+    # Logout method to clear the session #
+    def logout!
+      session.clear
+    end
+    
   end
 
 end
