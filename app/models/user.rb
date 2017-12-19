7afa5ddd10c15c7adf6be690f2551cf3fd99fef8
Janusz Szubert <theinvalidnonce@gmail.com>
2017-12-19 00:02:48 +0000

    Add has_secure_password to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :beers
   has_many :breweries
   
+  has_secure_password
+  
   validates :username, presence: true, uniqueness: true, length: { minimum: 4 }
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true, length: { minimum: 6 }
