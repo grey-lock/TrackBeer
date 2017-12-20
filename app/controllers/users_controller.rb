05bb89102b9fe79dcdc3fea03006c0202d19652a
Janusz Szubert <theinvalidnonce@gmail.com>
2017-12-20 17:36:53 +0000

    Fix username login requirement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   post '/login' do
     @user = User.find_by(username: params[:username])
     
-    if @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect '/users'
     else
